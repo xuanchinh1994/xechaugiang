ae3cb9248f8e3ce304c17b6b3b4307e28cd8261a
Chinh <xuanchinh1994@gmail.com>
2018-03-18 15:51:27 +0700

    fix issue utf8

--- mail_handler.php
+++ mail_handler.php
@@ -1,4 +1,9 @@
 <?php
+	function mail_utf8($to, $subject = '(No subject)', $message = '', $header = '') {
+	  $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+	  mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header);
+	}
+
 	$first_name=$_POST['first_name'];
 	$email=$_POST['email'];
 	$telephone=$_POST['telephone'];
@@ -9,9 +14,9 @@
 	$message="Name :".$first_name."\n"."Phone :".$telephone."\n"."Wrote the following :"."\n\n".$from;
 	var_dump($message);
 	// $headers="From: ".$email;
-	$headers = "From: " . $first_name . "<". $email .">\r\n".'Content-type: text/html; charset=UTF-8' .'\r\n';
+	$headers = "From: " . $first_name . "<". $email .">\r\n";
 
-	if(mail($to, $subject, $message, $headers)){
+	if(mail_utf8($to, $subject, $message, $headers)){
 		echo "Sent Successfully! Thank you"." ".$first_name.", We will contact you shortly!</h1>";
 	}
 	else{
