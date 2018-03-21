a106062955670308c07b1ffa0d05d0bfc79b2a48
Chinh <xuanchinh1994@gmail.com>
2018-03-21 13:46:07 +0700

    Content email

--- mail_handler.php
+++ mail_handler.php
@@ -1,6 +1,6 @@
 <?php
 	function mail_utf8($to, $subject = '(No subject)', $message = '', $header = '') {
-	  $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/html; charset=UTF-8' . "\r\n";
+	  $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
 	  mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header);
 	}
 
@@ -8,7 +8,7 @@
 	$email=$_POST['email'];
 	$telephone=$_POST['telephone'];
 	$from=$_POST['from'];
-	$to=$_POST['to'];
+	$to_1=$_POST['to'];
 	$startDate=$_POST['startDate'];
 	$endDate==$_POST['endDate'];
 	$selectCar==$_POST['state'];
@@ -16,21 +16,18 @@
 	$detail==$_POST['comment'];
 
 	$to='xuanchinh1994@gmail.com'; // Receiver Email ID, Replace with your email ID
-	$subject='Book Xe Online';
-	$message="
-	<html>
-	<head>
-	<title></title>
-	</head>
-	<body>
-	<p>Phiếu Đặt Xe Online</p>
-	<table>
-	<tr><th>Tên Khách Hàng</th><th>Số Điện Thoại</th><th>Email  </th><th>Điểm Đi</th><th>Điểm Đến</th><th>Thời Gian Đi</th><th>Thời Gian Về</th><th>Loại Xe    </th><th>Số Lượng Khách</th><th>Miêu Tả Chuyến Đi</th></tr>
-	<tr><td>.$first_name  </td><td>.$telephone  </td><td>.$email</td><td>.$from </td><td>.$to    </td><td>.$startDate </td><td>.$endDate   </td><td>.$selectCar</td><td>.$numPeople   </td><td>.$detail         </td></tr>
-	</table>
-	</body>
-	</html>
-	Name :".$first_name."\n"."Phone :".$telephone."\n"."Wrote the following :"."\n\n".$from;
+	$subject='BOOK XE ONLINE';
+	$message="TÊN KHÁCH HÀNG :".$first_name."\n"
+			."SỐ ĐT          :".$telephone."\n"
+			."EMAIL          :".$email."\n"
+			."ĐIỂM ĐI        :".$from."\n"
+			."ĐIỂM ĐẾN       :".$to_1."\n"
+			."THỜI GIAN ĐI   :".$startDate."\n"
+			."THỜI GIAN VỀ   :".$endDate."\n"
+			."LOẠI XE        :".$selectCar."\n"
+			."SỐ NGƯỜI       :".$numPeople."\n"
+			."CHI TIẾT       :".$detail."\n"
+			."-----------------------------"."\n";
 	var_dump($message);
 	// $headers="From: ".$email;
 	$headers = "From: " . $first_name . "<". $email .">\r\n";
